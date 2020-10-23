705c21d0d19358c3c26748e37963b39ae1e5e40c
haru315 <69494667+haru315@users.noreply.github.com>
2020-10-23 12:01:21 +0900

    Update __init__.py

--- test1023/__init__.py
+++ test1023/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 import azure.functions as func
 
-
+# tekitou
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
