30f1eb9dedd795908584ef2537cc0cbfe3f9bd63
haru315 <69494667+haru315@users.noreply.github.com>
2020-10-23 12:06:59 +0900

    Update __init__.py

--- test1023/__init__.py
+++ test1023/__init__.py
@@ -2,21 +2,24 @@ import logging
 
 import azure.functions as func
 
-# tekitou
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    first_name = req.params.get('first_name')
+    last_name = req.params.get('last_name')
+    if not (first_name and last_name):
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            first_name = req_body.get('first_name')
+            last_name = req_body.get('last_name')
+            
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if first_name and last_name:
+        return func.HttpResponse(f"こんにちは、{last_name}  {first_name}さん")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
